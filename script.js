2ee0e02b4f31bf58b6103b4e248df69f6f24bc66
admin <TimoninIV20@st.ithub.ru>
2021-05-30 21:36:03 +0300

    s

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-window.addEventListener("load", (e) => {
-    // Переменные карточек
+window.addEventListener("load", () => {
+    // Текст в header-е
+    var text = ["MISH"];
+    var count = 0;
+    var typi = "";
+    // Переменные карточек  
     const cards = document.querySelectorAll("#main-info, #screenshots, #project-info, #comment");
     // Для закрытия и открытия карт
     var card = "";
     var cardChild = "";
     var OpeningCard = false;
 
-    // Всякие функции
+    
+    //  Анимация набора текста в header-е
+    const TypeText = () => {
+        var interval = setTimeout( () => {
+            typi += text[0][count];
+            document.querySelector("#header-name").innerHTML = typi + "|";
+            count++;
+
+            if (count == text[0].length) {
+                clearTimeout(interval);
+                document.querySelector("#header-name").innerHTML = typi;
+                return true;
+            }
+        TypeText();
+        }, getRand(getRand(350*3.5)));
+    }
+    function getRand(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+    TypeText();
+    
+    // Открытие карточки
     const OpenCard = (e) => {
         if (OpeningCard == false) {
             OpeningCard = true;
@@ -24,17 +49,32 @@ window.addEventListener("load", (e) => {
             });
         }
     }
-
+    //  Закрытие карточки
     const CloseCard = (e) => {
         if (e.keyCode === 27 && cardChild.classList == "unHideSon") {
             OpeningCard = false;
             cardChild.classList.add("HideSon");
             cardChild.classList.remove("unHideSon");
             cards.forEach(i => {
-                i.style.cursor = "pointer";
+                i.style = null;
                 i.classList.remove("hideCard");
-            })        }
+            })
+        }
     }
+    
+    // Плавная прокрутка, которая работает галимо
+    document.querySelectorAll('a.scroll-to').forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault()
+            
+            const blockID = link.getAttribute('href')
+            
+            document.querySelector(blockID).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            })
+        })
+    })
 
 
     // Активаторы всяких функций
